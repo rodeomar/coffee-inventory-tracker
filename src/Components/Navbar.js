2119be291ed5d41a2e2f7a0c9ba4b9169f307b8c
Raed Alkhanbashi <raed.alkhanbashi@gmail.com>
2023-09-09 17:04:15 +0530

    Change class to `className`

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,28 +3,27 @@ import React from 'react';
 function Navbar() {
   return (
     <>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#qaa">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="/">Local Business Inventory Tracker</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="/">Home</a>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="/">Home</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="Beans">Beans</a>
+            <li className="nav-item">
+              <a className="nav-link" href="/Beans">Beans</a>
             </li>
- 
-            <li class="nav-item">
-              <a class="nav-link" href="Sacks">Coffee Sacks</a>
+            <li className="nav-item">
+              <a className="nav-link" href="/Sacks">Coffee Sacks</a>
             </li>
           </ul>
-          <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          <form className="form-inline my-2 my-lg-0">
+            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" id="searchInput" />
+            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
           </form>
         </div>
       </nav>
@@ -33,4 +32,3 @@ function Navbar() {
 }
 
 export default Navbar;
-
